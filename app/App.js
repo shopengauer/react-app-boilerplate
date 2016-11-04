6b19708a4349ff40d7682aab0829e53d79266b90
Pavlovens <localducks@gmail.com>
2016-11-04 15:21:57 +0400

    Работающее изменение state

--- app/App.js
+++ app/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {name: "Hello"}
+  }
+
+  tick() {
+    this.setState({
+      name: "Hello World"
+    });
+  }
+
+
   render(){
-    return (
-      <h1>Hello World</h1>
+     return (
+
+        <div>
+          <button onClick = {()=>{this.tick()}}>Revert</button>
+          <h1>{this.state.name}</h1>
+        </div>
     );
   }
 }
---
+++ components/Panel.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Pavlovens on 04.11.2016.
+ */
+"use strict"
